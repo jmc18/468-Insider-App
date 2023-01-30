66a5606625ad8434ed5ec783ad5eea8044168b15
Javier MC <javier.martinez@mexmax-internet.com>
2023-01-30 17:05:08 -0600

    Created clean architecture for backend project

---
+++ 468Insider/468Insider.Core/ApplicationServiceRegistration.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace _468Insider.Core
+{
+    public static class ApplicationServiceRegistration
+    {
+        /// <summary>
+        /// Registers the application services.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
