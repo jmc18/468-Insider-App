607a7826e912de86018710b856c63b10827467bb
Javier MC <javier.martinez@mexmax-internet.com>
2023-01-30 17:34:50 -0600

    Added application service to the program

--- 468Insider/468Insider.API/Program.cs
+++ 468Insider/468Insider.API/Program.cs
@@ -1,6 +1,11 @@
+using _468Insider.Core;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddApplicationServices();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
