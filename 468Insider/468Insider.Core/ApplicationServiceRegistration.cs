6215dd1d243cf63081649b74bdf37039cabf84ae
Javier MC <javier.martinez@mexmax-internet.com>
2023-02-23 16:00:04 -0600

    Updated code

--- 468Insider/468Insider.Core/ApplicationServiceRegistration.cs
+++ 468Insider/468Insider.Core/ApplicationServiceRegistration.cs
@@ -1,10 +1,14 @@
 ﻿using System.Reflection;
+using System.Text.Json.Serialization;
 using _468Insider.Core.Endpoints;
 using _468Insider.Infrastructure;
 using MediatR;
+using Microsoft.AspNetCore.Http.Json;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+
+using _468Insider.Domain.Helpers;
 
 namespace _468Insider.Core
 {
@@ -20,6 +24,15 @@ namespace _468Insider.Core
         {
             services.AddDbContext<Admin468InsiderDbContext>(options =>
                 options.UseSqlServer(configuration.GetConnectionString("468InsiderDB")));
+
+            services.Configure<JsonOptions>(options =>
+                options.SerializerOptions.DefaultIgnoreCondition =
+                    JsonIgnoreCondition.WhenWritingDefault | JsonIgnoreCondition.WhenWritingNull);
+
+            services.AddCors();
+
+            // configure strongly typed settings object
+            services.Configure<AppSettings>(configuration.GetSection("AppSettings"));
 
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
             services.AddMediatR(Assembly.GetExecutingAssembly());
