741707bc7f886733a3ddd3a26f721ed5baa1fc9d
Javier MC <mtzcornejo66@gmail.com>
2023-02-23 17:44:57 -0600

    Created BaseRepository class

---
+++ 468Insider/468Insider.Infrastructure/BaseRepository.cs
@@ -0,0 +1,45 @@
+﻿using _468Insider.Core.Interfaces.Persintence;
+using Microsoft.EntityFrameworkCore;
+
+namespace _468Insider.Infrastructure
+{
+    public class BaseRepository<T> : IAsyncRepository<T> where T : class
+    {
+
+        private readonly Admin468InsiderDbContext _dbContext;
+
+        public BaseRepository(Admin468InsiderDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public virtual async Task<T?> GetByIdAsync(Guid id)
+        {
+            return await _dbContext.Set<T>().FindAsync(id);
+        }
+
+        public virtual async Task<IReadOnlyList<T>> ListAllAsync()
+        {
+            return await _dbContext.Set<T>().ToListAsync();
+        }
+
+        public virtual async Task<T> AddAsync(T entity)
+        {
+            await _dbContext.Set<T>().AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public virtual async Task UpdateAsync(T entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public virtual async Task DeleteAsync(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
